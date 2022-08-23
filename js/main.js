c9a4a3f80d9c7f580788af52df57cbe8c1bc0d1f
AdmiralShakal <kirsan1998@mail.ru>
2022-08-23 08:12:42 +0300

    sliders&companies

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(function () {
+  var mixer = mixitup('.directions__programs-list');
+
+  $('.directions__filtre').on('click', () => {
+    $('.directions__filtre').removeClass('directions__filtre--active');
+    $(this).addClass('directions__filtre--active');
+  });
+
+  // sliders settings
+
+  $('.team__slider').slick({
+    arrows: false,
+    slidesToShow: 4,
+    infinite: true,
+    draggable: false,
+    appendArrows: $('.team__slider-arrows'),
+  });
+
+  $('.testimonials__slider').slick({
+    arrows: false,
+    dots: true,
+    draggable: false,
+    appendDots: $('.testimonials__dots'),
+  });
+
+  // sliders arrow functionality
+
+  $('.team__slider-prev').on('click', (e) => {
+    e.preventDefault();
+    $('.team__slider').slick('slickPrev');
+  });
+
+  $('.team__slider-next').on('click', (e) => {
+    e.preventDefault();
+    $('.team__slider').slick('slickNext');
+  });
+
+  $('.testimonials__prev').on('click', (e) => {
+    e.preventDefault();
+    $('.testimonials__slider').slick('slickPrev');
+  });
+
+  $('.testimonials__next').on('click', (e) => {
+    e.preventDefault();
+    $('.testimonials__slider').slick('slickNext');
+  });
+});
